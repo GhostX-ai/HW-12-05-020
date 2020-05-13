b649b586f625ea2048f0b4e0b507f2abaf6c1fe3
GhostX <egamovkhurshed29@gmail.com>
2020-05-14 04:28:23 +0500

    Second task has been done

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace HW12_05_2020
         {
             string text = "hello";
             string newtext = String.Join("", text.ToCharArray().Select(p => p == 'a' ? "1" : p == 'e' ? "2" : p == 'i' ? "3" : p == 'o' ? "4" : p == 'u' ? "5" : p == 'y' ? "6" : p.ToString()));
+            string lasttext = String.Join("", text.ToCharArray().Select(p => p == '1' ? "a" : p == '2' ? "e" : p == '3' ? "i" : p == '4' ? "o" : p == '5' ? "u" : p == '6' ? "y" : p.ToString()));
             Console.ReadKey();
         }
     }
